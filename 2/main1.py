1408c2d8fac389d5bf9fe3aec402ac839268643b
Matt Szelc <mattszelc@live.com>
2021-12-02 20:50:02 -0500

    Remove stupid stuff

--- 2/main1.py
+++ 2/main1.py
@@ -1,21 +1,19 @@
 import sys
 
-if __name__ == "__main__":
-	
-	a = []
-	with open(sys.argv[1], 'r') as fh:
-		for val in fh.readlines():
-			a.append(val.strip())
+a = []
+with open(sys.argv[1], 'r') as fh:
+    for val in fh.readlines():
+        a.append(val.strip())
 
-	h = 0
-	d = 0
-	for instr in a:
-		set = instr.split(' ')
-		if set[0] == 'forward':
-			h += int(set[1])
-		elif set[0] == 'down':
-			d += int(set[1])
-		elif set[0] == 'up':
-			d -= int(set[1])
+h = 0
+d = 0
+for instr in a:
+    set = instr.split(' ')
+    if set[0] == 'forward':
+        h += int(set[1])
+    elif set[0] == 'down':
+        d += int(set[1])
+    elif set[0] == 'up':
+        d -= int(set[1])
 
-	print(h*d)
+print(h*d)
--- 2/main2.py
+++ 2/main2.py
@@ -1,23 +1,21 @@
 import sys
 
-if __name__ == "__main__":
-	
-	a = []
-	with open(sys.argv[1], 'r') as fh:
-		for val in fh.readlines():
-			a.append(val.strip())
+a = []
+with open(sys.argv[1], 'r') as fh:
+    for val in fh.readlines():
+        a.append(val.strip())
 
-	h = 0
-	d = 0
-	aim = 0
-	for instr in a:
-		set = instr.split(' ')
-		if set[0] == 'forward':
-			h += int(set[1])
-			d += int(set[1]) * aim
-		elif set[0] == 'down':
-			aim += int(set[1])
-		elif set[0] == 'up':
-			aim -= int(set[1])
+h = 0
+d = 0
+aim = 0
+for instr in a:
+    set = instr.split(' ')
+    if set[0] == 'forward':
+        h += int(set[1])
+        d += int(set[1]) * aim
+    elif set[0] == 'down':
+        aim += int(set[1])
+    elif set[0] == 'up':
+        aim -= int(set[1])
 
-	print(h*d)
+print(h*d)

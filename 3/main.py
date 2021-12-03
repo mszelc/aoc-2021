4348a79b6534d12fafacc12b084d7151dd904518
Matt Szelc <mattszelc@live.com>
2021-12-03 00:45:35 -0500

    Function-atize common value finder, day 3

--- 3/main.py
+++ 3/main.py
@@ -6,34 +6,6 @@ with open(sys.argv[1], 'r') as fh:
     for val in fh.readlines():
         a.append(val.strip())
 
-b = list(a[0])
-half = int(len(a)/2)
-blen = len(a[0])
-first = True
-for val in a:
-    if first:
-        for i in range(0,blen):
-            b[i] = int(b[i])
-        first = False
-        continue
-    for i in range(0,blen):
-        b[i] += int(val[i])
-
-gstring = ''
-estring = ''
-for val in b:
-    if val > half:
-        gstring += '1'
-        estring += '0'
-    elif val < half:
-        gstring += '0'
-        estring += '1'
-    else:
-        gstring += '1'
-        estring += '0'
-
-print(int(gstring,2)*int(estring,2))
-
 a1 = deepcopy(a)
 a2 = deepcopy(a)
 for i in range(0,blen):
@@ -50,4 +22,38 @@ for i in range(0,blen):
         except ValueError:
             continue
 
+print(a1)
+print(a2)
 print(int(a1[0],2)*int(a2[0],2))
+
+def getCommons(a):
+    b = list(a[0])
+    half = int(len(a)/2)
+    blen = len(a[0])
+    first = True
+    for val in a:
+        if first:
+            for i in range(0,blen):
+                b[i] = int(b[i])
+            first = False
+            continue
+        for i in range(0,blen):
+            b[i] += int(val[i])
+
+    gstring = ''
+    estring = ''
+    for val in b:
+        if val > half:
+            gstring += '1'
+            estring += '0'
+        elif val < half:
+            gstring += '0'
+            estring += '1'
+        else:
+            gstring += '1'
+            estring += '0'
+
+    #print(int(gstring,2)*int(estring,2))
+    return gstring, estring
+
+

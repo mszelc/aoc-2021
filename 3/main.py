ebfb53f4d6695ed86159461b414a07b235d6bfd8
Matt Szelc <mattszelc@live.com>
2021-12-03 20:46:48 -0500

    Make day 3 actually work!

--- 3/main.py
+++ 3/main.py
@@ -1,34 +1,9 @@
 import sys
 from copy import deepcopy
 
-a = []
-with open(sys.argv[1], 'r') as fh:
-    for val in fh.readlines():
-        a.append(val.strip())
-
-a1 = deepcopy(a)
-a2 = deepcopy(a)
-for i in range(0,blen):
-    gbit = gstring[i]
-    ebit = estring[i]
-    for val in a:
-        try:
-            if val[i] != gbit:
-                if len(a1) > 1:
-                    a1.remove(val)
-            if val[i] != ebit:
-                if len(a2) > 1:
-                    a2.remove(val)
-        except ValueError:
-            continue
-
-print(a1)
-print(a2)
-print(int(a1[0],2)*int(a2[0],2))
-
 def getCommons(a):
     b = list(a[0])
-    half = int(len(a)/2)
+    half = float(len(a)/2)
     blen = len(a[0])
     first = True
     for val in a:
@@ -55,5 +30,56 @@ def getCommons(a):
 
     #print(int(gstring,2)*int(estring,2))
     return gstring, estring
+
+a = []
+with open(sys.argv[1], 'r') as fh:
+    for val in fh.readlines():
+        a.append(val.strip())
+
+#a = ['00100',
+#     '11110',
+#     '10110',
+#     '10111',
+#     '10101',
+#     '01111',
+#     '00111',
+#     '11100',
+#     '10000',
+#     '11001',
+#     '00010',
+#     '01010']
+
+most, least = getCommons(a)
+mc = most[0]
+lc = least[0]
+print(int(most,2)*int(least,2))
+
+a1 = a2 = a
+for i in range(0,len(most)):
+    a1c = deepcopy(a1)
+    a2c = deepcopy(a2)
+    if len(a1) > 1:
+        for val in a1:
+            if val[i] != mc:
+                if len(a1) > 1:
+                    a1c.remove(val)
+    if len(a2) > 1:
+        for val in a2:
+            if val[i] != lc:
+                if len(a2) > 1:
+                    a2c.remove(val)
+    a1 = a1c
+    a2 = a2c
+    most, _ = getCommons(a1)
+    _, least = getCommons(a2)
+    try:
+        mc = most[i+1]
+        lc = least[i+1]
+    except IndexError:
+        break
+
+print(int(a1[0],2)*int(a2[0],2))
+
+
 
 
